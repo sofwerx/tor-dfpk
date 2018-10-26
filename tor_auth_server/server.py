bdcbae2582e951c31445fae9af01f78207e6683f
Mason Ashbridge <masonjash@gmail.com>
2018-10-26 16:31:27 +0000

    Make server read a file for auth credentials

--- tor_auth_server/server.py
+++ tor_auth_server/server.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import re
 
 
 # torrc configuration data which tells clients how to connect to the private
@@ -15,6 +16,14 @@ TORRC_CONFIG_DATA = {
         'fingerprint': 'afdsafdasf'
     },
 }
+
+
+AUTH_FILENAME = 'auth_credentials.dat'
+AUTH_CREDENTIALS = []
+
+# Regex used to check if a string is indeed base64
+BASE64_REGEX_VALIDATOR = r'^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$'
+BASE64_REGEX_VALIDATOR = re.compile(BASE64_REGEX_VALIDATOR)
 
 
 class TorAuthServer(BaseHTTPRequestHandler):
@@ -35,19 +44,41 @@ class TorAuthServer(BaseHTTPRequestHandler):
             # User is not logged in and must post with basic auth headers
             self.send_auth_request()
             self.wfile.write(b'Please enter your username and password')
-        elif auth_header == 'Basic dGVzdDp0ZXN0':
-            # dGVzdDp0ZXN0 is base64 for test:test
-            # TODO: parse out the base64 string and authenticate it.
-            print('Authenticated user connected, sending TOR config')
-            self.send_response(200)
-            self.send_tor_config_data()
+        elif auth_header.startswith('Basic '):
+            auth_token = auth_header.split(' ')[1]
+            print(auth_token)
+            if auth_token in AUTH_CREDENTIALS:
+                print('Authenticated user connected, sending TOR config')
+                self.send_response(200)
+                self.send_tor_config_data()
+            else:
+                self.send_auth_request()
+                self.wfile.write(b'Invalid auth credentials, try again')
         else:
             self.send_auth_request()
             self.wfile.write(b'Invalid auth credentials, try again')
 
 
+def is_valid_base64(content):
+    return BASE64_REGEX_VALIDATOR.match(content) is not None
+
+
+def read_auth_file():
+    with open(AUTH_FILENAME, 'r') as f:
+        for line in f.readlines():
+            if line:
+                line = line.strip()
+                if is_valid_base64(line):
+                    AUTH_CREDENTIALS.append(line)
+                else:
+                    print(f'auth credential {line} ignored, must be valid base64')
+
+
 def run():
     print('Starting TOR Auth server')
+
+    read_auth_file()
+
     server_address = ('127.0.0.1', 8080)
     httpd = HTTPServer(server_address, TorAuthServer)
 
